07214854e4f46d58483e394e6e0b79c5efe59ca9
EmmanuelNyameke <155826955+EmmanuelNyameke@users.noreply.github.com>
2024-10-20 15:03:13 +0000

    Add files via upload

---
+++ SoftBall/frmSoftball.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmSoftball
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        mnuFile = New ToolStripMenuItem()
+        mnuClear = New ToolStripMenuItem()
+        mnuExit = New ToolStripMenuItem()
+        lblHeading = New Label()
+        btnScore = New Button()
+        lblTotalFinalScore = New Label()
+        lstScore = New ListBox()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {mnuFile})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(675, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' mnuFile
+        ' 
+        mnuFile.DropDownItems.AddRange(New ToolStripItem() {mnuClear, mnuExit})
+        mnuFile.Name = "mnuFile"
+        mnuFile.Size = New Size(37, 20)
+        mnuFile.Text = "File"
+        ' 
+        ' mnuClear
+        ' 
+        mnuClear.Name = "mnuClear"
+        mnuClear.Size = New Size(101, 22)
+        mnuClear.Text = "Clear"
+        ' 
+        ' mnuExit
+        ' 
+        mnuExit.Name = "mnuExit"
+        mnuExit.Size = New Size(101, 22)
+        mnuExit.Text = "Exit"
+        ' 
+        ' lblHeading
+        ' 
+        lblHeading.AutoSize = True
+        lblHeading.Font = New Font("Tahoma", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblHeading.Location = New Point(165, 24)
+        lblHeading.Name = "lblHeading"
+        lblHeading.Size = New Size(282, 33)
+        lblHeading.TabIndex = 1
+        lblHeading.Text = "Softball Scoreboard"
+        ' 
+        ' btnScore
+        ' 
+        btnScore.Font = New Font("Tahoma", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnScore.ForeColor = Color.Gray
+        btnScore.Location = New Point(232, 99)
+        btnScore.Name = "btnScore"
+        btnScore.Size = New Size(150, 34)
+        btnScore.TabIndex = 2
+        btnScore.Text = "Enter Score"
+        btnScore.UseVisualStyleBackColor = True
+        ' 
+        ' lblTotalFinalScore
+        ' 
+        lblTotalFinalScore.AutoSize = True
+        lblTotalFinalScore.Font = New Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTotalFinalScore.ForeColor = Color.Gray
+        lblTotalFinalScore.Location = New Point(165, 184)
+        lblTotalFinalScore.Name = "lblTotalFinalScore"
+        lblTotalFinalScore.Size = New Size(92, 29)
+        lblTotalFinalScore.TabIndex = 3
+        lblTotalFinalScore.Text = "Label1"
+        lblTotalFinalScore.Visible = False
+        ' 
+        ' lstScore
+        ' 
+        lstScore.Font = New Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lstScore.FormattingEnabled = True
+        lstScore.ItemHeight = 19
+        lstScore.Location = New Point(542, 114)
+        lstScore.Name = "lstScore"
+        lstScore.Size = New Size(41, 156)
+        lstScore.TabIndex = 4
+        ' 
+        ' frmSoftball
+        ' 
+        AcceptButton = btnScore
+        AutoScaleDimensions = New SizeF(7F, 14F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        BackgroundImage = My.Resources.Resources.Softball
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(675, 419)
+        Controls.Add(lstScore)
+        Controls.Add(lblTotalFinalScore)
+        Controls.Add(btnScore)
+        Controls.Add(lblHeading)
+        Controls.Add(MenuStrip1)
+        Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        MainMenuStrip = MenuStrip1
+        Name = "frmSoftball"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Softball Scoreboard"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents btnScore As Button
+    Friend WithEvents lblTotalFinalScore As Label
+    Friend WithEvents lstScore As ListBox
+
+End Class
---
+++ SoftBall/frmSoftball.vb
@@ -0,0 +1,55 @@
+﻿' Program Name: Softball Scoreboard
+' Date: October 20, 2024
+' Author: K Bola
+' Purpose: This application calculates the scores of each inning during a softball game for one team 
+
+Public Class frmSoftball
+    Private Sub frmSoftball_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lstScore.Items.Clear()
+    End Sub
+    Private Sub btnScore_Click(sender As Object, e As EventArgs) Handles btnScore.Click
+        Dim strScore As String
+        Dim decScore As Decimal
+        Dim decTotalScore As Decimal = 0
+        Dim decFinalScore As Decimal
+        Dim strInputMessage As String = "Enter Score: "
+        Dim strInputHeading As String = "Scoreboard"
+        Dim strNonNumericValue As String = "Error - Enter a Valid Value: "
+        Dim strNegativeValue As String = "Error - Enter a Positive Value: "
+        Dim strCancelClicked As String = ""
+        Dim intNumberOfEntries As Integer = 1
+        Dim intMaxNumberOfEntries As Integer = 7
+        strScore = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, "")
+        Do Until intNumberOfEntries > intMaxNumberOfEntries Or strScore = strCancelClicked
+            If IsNumeric(strScore) Then
+                decScore = Convert.ToDecimal(strScore)
+                If decScore > 0 Then
+                    lstScore.Items.Add(decScore)
+                    decTotalScore += decScore
+                    intNumberOfEntries += 1
+                Else
+                    strInputMessage = strNegativeValue
+                End If
+            Else
+                strInputMessage = strNonNumericValue
+            End If
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strScore = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, "")
+            End If
+        Loop
+        If intNumberOfEntries > 1 Then
+            decFinalScore = decTotalScore
+            lblTotalFinalScore.Visible = True
+            lblTotalFinalScore.Text = "Total Final Score: " & decFinalScore
+        End If
+        btnScore.Enabled = False
+    End Sub
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        lstScore.Items.Clear()
+        lblTotalFinalScore.Text = ""
+        btnScore.Enabled = True
+    End Sub
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        Close()
+    End Sub
+End Class
